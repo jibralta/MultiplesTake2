309fb60db7eb5851dd2b6f4de9d0a4af269ed215
Joy Umali <joyumali@gmail.com>
2016-09-16 19:21:11 -0700

    added initial files

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+
+    // Outlets on first screen
+    @IBOutlet weak var logoImg: UIImageView!
+    
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    // Outlets on second screen (after Play Button Tapped)
+    
+    @IBOutlet weak var addText: UILabel!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    // Global Properties
+    var sum = 0
+    var currentSum = 0
+    
+    func multipleEqn() -> String {
+        
+        let multiple = Int(whatMultipleTxt.text!)!
+        let currentSum = sum + multiple
+        //sum = currentSum
+        
+        let equation = "\(sum) + \(multiple) = \(currentSum)"
+        return (equation)
+        
+        //return ("\(sum) + \(multiple) = \(currentSum)")
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    // Actions!!
+    
+    
+    @IBAction func tapPlayButton(sender: AnyObject) {
+        
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != "" {
+            
+            logoImg.hidden = true
+            whatMultipleTxt.hidden = true
+            playButton.hidden = true
+            
+            addText.hidden = false
+            addButton.hidden = false
+   
+        }
     }
+    
+    
+    @IBAction func tapAddButton(sender: AnyObject) {
+        
+        addText.text = multipleEqn()
+        //addText.text = String(multipleEqn().currentSum)
+        //sum = currentSum
+        
+        
+    }
+    
 
+    
 
 }
 
